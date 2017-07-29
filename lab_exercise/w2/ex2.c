aa65a9a9560c651d8b95b2dff3724d349ec08582
Sok Ang Ho <s3569963@csitprdap01.int.its.rmit.edu.au>
2017-07-30 00:24:32 +1000

    update on week 2 exercise 2

--- lab_exercise/w2/ex2.c
+++ lab_exercise/w2/ex2.c
@@ -7,26 +7,29 @@ int main(void) {
 	int num, x;
 
 	printf("Please enter a positive number: ");
-	
-	if(scanf("%d", &num) != 1) {
-		printf("Error: scanf() failed! Program Aborting!\n\n");
+	scanf("%d", &num);	
+
+	if(num >= 0) {
+		printf("\n%-10s%-10s%-8s%-10s%-10s\n", "Number", "Square", "Cube", "Quartic", "Quintic");
+		UNDERLINE;
+
+		for(int i=0; i<=num; i++) {
+			x=i; printf("%-10d", x);
+			x*=i; printf("%-10d", x);
+			x*=i; printf("%-10d", x);
+			x*=i; printf("%-10d", x);
+			x*=i; printf("%-10d", x);
+		
+			printf("\n");
+		}
+
+	UNDERLINE;
 		return EXIT_FAILURE;
 	}
 
-	printf("\n%-10s%-10s%-8s%-10s%-10s\n", "Number", "Square", "Cube", "Quartic", "Quintic");
-	UNDERLINE;
-
-	for(int i=0; i<=num; i++) {
-		x=i; printf("%-10d", x);
-		x*=i; printf("%-10d", x);
-		x*=i; printf("%-10d", x);
-		x*=i; printf("%-10d", x);
-		x*=i; printf("%-10d", x);
-	
-		printf("\n");
+	else {
+		printf("Error: scanf() failed! Program Aborting!\n\n");
 	}
-
-	UNDERLINE;
 
 	return EXIT_SUCCESS;
 }
